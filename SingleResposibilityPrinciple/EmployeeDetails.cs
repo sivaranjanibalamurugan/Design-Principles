14dd0cee23133c0fa1d641f7f405b5a68323d63e
unknown <sivaranjanib99@gmail.com>
2021-08-02 16:51:25 +0530

    [siva]UC1 creating simple responsibility principle

---
+++ SingleResposibilityPrinciple/EmployeeDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResposibilityPrinciple
+{
+    class EmployeeDetails
+    {
+        // contains the variable list
+      
+            public string empName { get; set; }
+            public int empId { get; set; }
+            public int empAge { get; set; }
+        
+    }
+}
---
+++ SingleResposibilityPrinciple/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace SingleResposibilityPrinciple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Single Responsibility Principle");
+            List<EmployeeDetails> list = null;
+            OperationManagement operation = new OperationManagement();
+            for (int i = 0; i < 5; i++)
+            {
+
+                list = operation.ReadInput();
+            }
+            GenerateReport generate = new GenerateReport();
+            generate.GenerateEmployeeReport(list);
+        }
+    }
+}
